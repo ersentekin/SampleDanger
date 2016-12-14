6d3a5f1e17bc0debead6fdb91ec16ee80fb647f6
Ersen Tekin <ersen.tekin@gmail.com>
2016-12-14 13:47:32 +0100

    dummy change

--- SampleDanger/ViewController.m
+++ SampleDanger/ViewController.m
@@ -14,9 +14,9 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+-(void ) viewDidLoad {
     [super viewDidLoad];
-    NSLog(@"perfect change!");
+    NSLog(  @"perfect change!")   ;
     // Do any additional setup after loading the view, typically from a nib.
 }
 
