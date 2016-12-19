cbf4c7cd0564b2bd1cd42b26065b5f9c52a6609b
Ersen Tekin <ersen.tekin@gmail.com>
2016-12-19 17:34:56 +0100

    Adds gem

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'danger'
 gem 'pry'
 gem 'git_diff_parser'
+gem 'danger-code_style_validation', :git => 'https://github.com/ersentekin/danger-plugin.git'
--- SampleDanger/ViewController.m
+++ SampleDanger/ViewController.m
@@ -1,13 +1,3 @@
-//
-//  ViewController.m
-//  SampleDanger
-//
-//  Created by Ersen Tekin on 07/12/2016.
-//  Copyright © 2016 Ersen Tekin. All rights reserved.
-//
-
-#import "ViewController.h"
-
 @interface ViewController ()
 
 @end
@@ -19,12 +9,5 @@
     NSLog(@"perfect change!");
     // Do any additional setup after loading the view, typically from a nib.
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 
 @end
